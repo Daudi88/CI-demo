476e490bffd209e9c89b930e2d7bf30ce8ec7aca
Daudi88 <david.strom88@gmail.com>
2021-11-05 11:35:25 +0100

    I am committed to you

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CI_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
         }
     }
 }
